a347bc7ea9b84018527f50b1bdd83b634135cd6d
gregorsunta <gresunta@gmail.com>
2023-03-17 15:55:22 +0100

    Setup webpack and add loaders
    Set webpack entry and output for a specific file structure.
    Add source map for easier error tracking
    Add loaders:
    - Babel for older browser support (ES6 modules)
    - CSS and Style loaders
    - Other asset loaders such ass fonts and svgs.

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+import * as path from 'path';
+const __dirname = path.resolve();
+
+export default {
+  entry: './src/index.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  watch: true,
+  output: {
+    filename: '[name].bundle.js',
+    path: path.join(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: ['/' + '/node_modules'],
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(ttf|eot|svg)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: './fonts/[name][ext]',
+        },
+      },
+    ],
+  },
+};
